431b27752d764df4a7d067a4811b4cbf306ed08c
Sanjay Purswani <sanjsanj@hotmail.com>
2018-09-18 19:14:47 +0100

    add post todo route

---
+++ server/models/user.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+const User = mongoose.model("User", {
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+    minlength: 1
+  }
+});
+
+module.exports = {
+  User
+}
--- server/server.js
+++ server/server.js
@@ -1,56 +1,32 @@
-const mongoose = require("mongoose");
+const express = require("express");
+const bodyParser = require("body-parser");
 
-mongoose.Promise = global.Promise;
-mongoose.connect("mongodb://localhost:27017/TodoApp");
+const { mongoose } = require("./db/mongoose");
+const { Todo } = require("./models/todo");
+const { User } = require("./models/user");
 
-const Todo = mongoose.model("Todo", {
-  text: {
-    type: String,
-    required: true,
-    minlength: 1,
-    trim: true
-  },
-  completed: {
-    type: Boolean,
-    default: false
-  },
-  completedAt: {
-    type: Number,
-    default: null
-  }
+const app = express();
+
+app.use(bodyParser.json());
+
+app.post("/todos", (req, res) => {
+  const todo = new Todo({
+    text: req.body.text
+  });
+
+  todo
+    .save()
+    .then(
+      doc => {
+        res.send(doc);
+      }
+      // e => {
+      //   res.status(400).send(e);
+      // }
+    )
+    .catch(e => res.status(400).send(e));
 });
 
-const newTodo = new Todo({
-  text: "Blah blah blah"
+app.listen(3000, () => {
+  console.log("Started on port 3000");
 });
-
-newTodo.save().then(
-  doc => {
-    console.log("Saved:", doc);
-  },
-  e => {
-    console.log("Error saving:".e);
-  }
-);
-
-const User = mongoose.model("User", {
-  email: {
-    type: String,
-    required: true,
-    trim: true,
-    minlength: 1
-  }
-});
-
-const newUser = new User({
-  email: "a@a.com"
-});
-
-newUser.save().then(
-  doc => {
-    console.log("Saved:", doc);
-  },
-  e => {
-    console.log("Error:", e);
-  }
-);

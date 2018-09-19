3de319b68755a476a0a5012a1364d387f07a64b5
Sanjay Purswani <sanjsanj@hotmail.com>
2018-09-19 20:05:12 +0100

    add GET /todos route

--- server/server.js
+++ server/server.js
@@ -16,11 +16,17 @@ app.post("/todos", (req, res) => {
 
   todo
     .save()
-    .then(
-      doc => res.send(doc)
-    )
+    .then(doc => res.send(doc))
     .catch(e => res.status(400).send(e));
 });
+
+app.get("/todos", (req, res) => {
+  Todo.find()
+    .then((todos) => {
+      res.send({ todos })
+    })
+    .catch(e => res.status(400).send(e))
+})
 
 app.listen(3000, () => {
   console.log("Started on port 3000");
@@ -28,4 +34,4 @@ app.listen(3000, () => {
 
 module.exports = {
   app
-}
+};

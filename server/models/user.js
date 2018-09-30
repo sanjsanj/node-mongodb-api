3d0d1a7b22d6e04490be6913899f02bc88387d15
Sanjay Purswani <sanjsanj@hotmail.com>
2018-09-30 12:54:07 +0100

    add DELETE /users/me/token

--- server/models/user.js
+++ server/models/user.js
@@ -52,6 +52,18 @@ UserSchema.methods.generateAuthToken = function(user) {
   return user.save().then(() => token);
 };
 
+UserSchema.methods.removeToken = function(token) {
+  const user = this;
+
+  return user.update({
+    $pull: {
+      tokens: {
+        token: token
+      }
+    }
+  });
+};
+
 UserSchema.statics.findByToken = function(token) {
   const User = this;
   let decoded;
